d38bb1b663896e9594d438cf015556166122dc2f
tharuvetri <69648056+tharuvetri@users.noreply.github.com>
2022-09-08 13:09:59 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Fact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =5;
+		int fact = 1;
+		for(i=1;i<=5;i++)
+		{
+			fact = fact * i;
+		}
+System.out.println(fact);
+	}
+
+}
